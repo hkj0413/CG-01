50de8ccc936ef82203ab94c0a2c8229ab5bfbf25
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-09-24 03:18:52 +0900

    0-6 생성

---
+++ 0-6/main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#pragma warning(disable:6031)
+
+int main(void)
+{
+
+
+	return 0;
+}
