5abadc57192835845d6dc7bfe8ea4765f0b960f7
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-09-24 07:20:11 +0900

    0-7 끝

--- 0-7/main.c
+++ 0-7/main.c
@@ -9,7 +9,7 @@ void PrintBoard(int n[30][30]);
 
 int main(void)
 {
-	int start = 0, x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+	int start = 0, x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, x4 = 0, y4 = 0;
 
 	PrintBoard(board);
 
@@ -19,9 +19,15 @@ int main(void)
 		{
 			int temp = 0;
 
-			printf("input coord value : ");
+			printf("input coord value1 : ");
 
 			scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+			getchar();
+
+			printf("input coord value2 : ");
+
+			scanf("%d %d %d %d", &x3, &y3, &x4, &y4);
 
 			if (x1 > x2)
 			{
@@ -37,15 +43,37 @@ int main(void)
 				y2 = temp;
 			}
 
-			if (x1 >= 0 && x2 < 30 && (x1 != y1 || x2 != y2))
+			if (x3 > x4)
 			{
-				if (y1 >= 0 && y2 < 30)
+				temp = x3;
+				x3 = x4;
+				x4 = temp;
+			}
+
+			if (y3 > y4)
+			{
+				temp = y3;
+				y3 = y4;
+				y4 = temp;
+			}
+
+			if (x1 >= 0 && x2 < 30 && (x1 != y1 || x2 != y2) && x3 >= 0 && x4 < 30 && (x3 != y3 || x4 != y4))
+			{
+				if (y1 >= 0 && y2 < 30 && y3 >= 0 && y4 < 30)
 				{
 					for (int i = y1; i <= y2; i++)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
+						}
+					}
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] += 2;
 						}
 					}
 				}
@@ -53,7 +81,7 @@ int main(void)
 
 			else
 			{
-				printf("Error (0 <= n <= 29, x1 != x2 and y1 != y2)\n");
+				printf("Error (0 <= n <= 29, x1 != x2 and y1 != y2 and x3 != x4 and y3 != y4)\n");
 
 				continue;
 			}
@@ -75,15 +103,15 @@ int main(void)
 
 			if (order == 'x')
 			{
-				for (int i = 0; i < 30; i++)
+				for (int i = y1; i <= y2; i++)
 				{
-					for (int j = 0; j < 30; j++)
+					for (int j = x1; j <= x2; j++)
 					{
-						board[i][j] = 0;
+						board[i][j] -= 1;
 					}
 				}
 
-				if (x2 > x1 && x2 < 29)
+				if (x2 >= x1 && x2 < 29)
 				{
 					x1 += 1;
 
@@ -93,12 +121,26 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
 						}
 					}
 				}
 
-				else if (x2 == 29)
+				else if (x2 == 29 && x1 == 29)
+				{
+					x1 = 0;
+
+					x2 = 0;
+
+					for (int i = y1; i <= y2; i++)
+					{
+						board[i][0] += 1;
+
+						board[i][29] -= 1;
+					}
+				}
+
+				else if (x2 == 29 && x1 != 29)
 				{
 					x1 += 1;
 
@@ -108,7 +150,7 @@ int main(void)
 					{
 						for (int j = 0; j <= 29; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
 						}
 					}
 
@@ -116,12 +158,12 @@ int main(void)
 					{
 						for (int j = x2 + 1; j < x1; j++)
 						{
-							board[i][j] = 0;
+							board[i][j] -= 1;
 						}
 					}
 				}
 
-				else if (x1 == 29)
+				else if (x1 == 29 && x2 != 29)
 				{
 					x1 = 0;
 
@@ -129,9 +171,14 @@ int main(void)
 
 					for (int i = y1; i <= y2; i++)
 					{
+						board[i][29] -= 1;
+
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							if (board[i][j] != 1)
+							{
+								board[i][j] += 1;
+							}
 						}
 					}
 				}
@@ -146,7 +193,10 @@ int main(void)
 					{
 						for (int j = 0; j <= 29; j++)
 						{
-							board[i][j] = 1;
+							if (board[i][j] != 1)
+							{
+								board[i][j] += 1;
+							}
 						}
 					}
 
@@ -154,7 +204,7 @@ int main(void)
 					{
 						for (int j = x2 + 1; j < x1; j++)
 						{
-							board[i][j] = 0;
+							board[i][j] -= 1;
 						}
 					}
 				}
@@ -162,15 +212,15 @@ int main(void)
 
 			else if (order == 'X')
 			{
-				for (int i = 0; i < 30; i++)
+				for (int i = y1; i <= y2; i++)
 				{
-					for (int j = 0; j < 30; j++)
+					for (int j = x1; j <= x2; j++)
 					{
-						board[i][j] = 0;
+						board[i][j] -= 1;
 					}
 				}
 
-				if (x2 > x1 && x1 > 0)
+				if (x2 >= x1 && x1 > 0)
 				{
 					x1 -= 1;
 
@@ -180,12 +230,26 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
 						}
 					}
 				}
 
-				else if (x1 == 0)
+				else if (x2 == 0 && x1 == 0)
+				{
+					x1 = 29;
+
+					x2 = 29;
+
+					for (int i = y1; i <= y2; i++)
+					{
+						board[i][29] += 1;
+
+						board[i][0] -= 1;
+					}
+				}
+
+				else if (x1 == 0 && x2 != 0)
 				{
 					x1 = 29;
 
@@ -195,7 +259,7 @@ int main(void)
 					{
 						for (int j = 0; j <= 29; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
 						}
 					}
 
@@ -203,12 +267,12 @@ int main(void)
 					{
 						for (int j = x2 + 1; j < x1; j++)
 						{
-							board[i][j] = 0;
+							board[i][j] -= 1;
 						}
 					}
 				}
 
-				else if (x2 == 0)
+				else if (x2 == 0 && x1 != 0)
 				{
 					x1 -= 1;
 
@@ -216,9 +280,14 @@ int main(void)
 
 					for (int i = y1; i <= y2; i++)
 					{
+						board[i][0] -= 1;
+
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							if (board[i][j] != 1)
+							{
+								board[i][j] += 1;
+							}
 						}
 					}
 				}
@@ -233,7 +302,10 @@ int main(void)
 					{
 						for (int j = 0; j <= 29; j++)
 						{
-							board[i][j] = 1;
+							if (board[i][j] != 1)
+							{
+								board[i][j] += 1;
+							}
 						}
 					}
 
@@ -241,7 +313,7 @@ int main(void)
 					{
 						for (int j = x2 + 1; j < x1; j++)
 						{
-							board[i][j] = 0;
+							board[i][j] -= 1;
 						}
 					}
 				}
@@ -249,15 +321,15 @@ int main(void)
 
 			else if (order == 'y')
 			{
-				for (int i = 0; i < 30; i++)
+				for (int i = y1; i <= y2; i++)
 				{
-					for (int j = 0; j < 30; j++)
+					for (int j = x1; j <= x2; j++)
 					{
-						board[i][j] = 0;
+						board[i][j] -= 1;
 					}
 				}
 
-				if (y2 > y1 && y2 < 29)
+				if (y2 >= y1 && y2 < 29)
 				{
 					y1 += 1;
 
@@ -267,12 +339,26 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
 						}
 					}
 				}
 
-				else if (y2 == 29)
+				else if (y2 == 29 && y1 == 29)
+				{
+					y1 = 0;
+
+					y2 = 0;
+
+					for (int i = x1; i <= x2; i++)
+					{
+						board[0][i] += 1;
+
+						board[29][i] -= 1;
+					}
+				}
+
+				else if (y2 == 29 && y1 != 29)
 				{
 					y1 += 1;
 
@@ -282,7 +368,7 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
 						}
 					}
 
@@ -290,12 +376,12 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 0;
+							board[i][j] -= 1;
 						}
 					}
 				}
 
-				else if (y1 == 29)
+				else if (y1 == 29 && y2 != 29)
 				{
 					y1 = 0;
 
@@ -305,7 +391,12 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[29][j] -= 1;
+
+							if (board[i][j] != 1)
+							{
+								board[i][j] += 1;
+							}
 						}
 					}
 				}
@@ -320,7 +411,10 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							if (board[i][j] != 1)
+							{
+								board[i][j] += 1;
+							}
 						}
 					}
 
@@ -328,7 +422,7 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 0;
+							board[i][j] -= 1;
 						}
 					}
 				}
@@ -336,15 +430,15 @@ int main(void)
 
 			else if (order == 'Y')
 			{
-				for (int i = 0; i < 30; i++)
+				for (int i = y1; i <= y2; i++)
 				{
-					for (int j = 0; j < 30; j++)
+					for (int j = x1; j <= x2; j++)
 					{
-						board[i][j] = 0;
+						board[i][j] -= 1;
 					}
 				}
 
-				if (y2 > y1 && y1 > 0)
+				if (y2 >= y1 && y1 > 0)
 				{
 					y1 -= 1;
 
@@ -354,12 +448,26 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
 						}
 					}
 				}
 
-				else if (y1 == 0)
+				else if (y2 == 0 && y1 == 0)
+				{
+					y1 = 29;
+
+					y2 = 29;
+
+					for (int i = x1; i <= x2; i++)
+					{
+						board[29][i] += 1;
+
+						board[0][i] -= 1;
+					}
+				}
+
+				else if (y1 == 0 && y2 != 0)
 				{
 					y1 = 29;
 
@@ -369,7 +477,7 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[i][j] += 1;
 						}
 					}
 
@@ -377,12 +485,12 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 0;
+							board[i][j] -= 1;
 						}
 					}
 				}
 
-				else if (y2 == 0)
+				else if (y2 == 0 && y1 != 0)
 				{
 					y1 -= 1;
 
@@ -392,7 +500,12 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							board[0][j] -= 1;
+
+							if (board[i][j] != 1)
+							{
+								board[i][j] += 1;
+							}
 						}
 					}
 				}
@@ -407,7 +520,10 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 1;
+							if (board[i][j] != 1)
+							{
+								board[i][j] += 1;
+							}
 						}
 					}
 
@@ -415,7 +531,7 @@ int main(void)
 					{
 						for (int j = x1; j <= x2; j++)
 						{
-							board[i][j] = 0;
+							board[i][j] -= 1;
 						}
 					}
 				}
@@ -423,6 +539,14 @@ int main(void)
 
 			else if (order == 's')
 			{
+				for (int i = y1; i <= y2; i++)
+				{
+					for (int j = x1; j <= x2; j++)
+					{
+						board[i][j] -= 1;
+					}
+				}
+
 				if (x1 > 0)
 				{
 					x1 -= 1;
@@ -447,18 +571,18 @@ int main(void)
 				{
 					for (int j = x1; j <= x2; j++)
 					{
-						board[i][j] = 1;
+						board[i][j] += 1;
 					}
 				}
 			}
 
 			else if (order == 'S')
 			{
-				for (int i = 0; i < 30; i++)
+				for (int i = y1; i <= y2; i++)
 				{
-					for (int j = 0; j < 30; j++)
+					for (int j = x1; j <= x2; j++)
 					{
-						board[i][j] = 0;
+						board[i][j] -= 1;
 					}
 				}
 
@@ -490,7 +614,525 @@ int main(void)
 				{
 					for (int j = x1; j <= x2; j++)
 					{
-						board[i][j] = 1;
+						board[i][j] += 1;
+					}
+				}
+			}
+
+			else if (order == 'w')
+			{
+				for (int i = y3; i <= y4; i++)
+				{
+					for (int j = x3; j <= x4; j++)
+					{
+						board[i][j] -= 2;
+					}
+				}
+
+				if (x4 >= x3 && x4 < 29)
+				{
+					x3 += 1;
+
+					x4 += 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] += 2;
+						}
+					}
+				}
+
+				else if (x4 == 29 && x3 == 29)
+				{
+					x3 = 0;
+
+					x4 = 0;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						board[i][0] += 2;
+
+						board[i][29] -= 2;
+					}
+				}
+
+				else if (x4 == 29 && x3 != 29)
+				{
+					x3 += 1;
+
+					x4 = 0;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = 0; j <= 29; j++)
+						{
+							board[i][j] += 2;
+						}
+					}
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x4 + 1; j < x3; j++)
+						{
+							board[i][j] -= 2;
+						}
+					}
+				}
+
+				else if (x3 == 29 && x4 != 29)
+				{
+					x3 = 0;
+
+					x4 += 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						board[i][29] -= 2;
+
+						for (int j = x3; j <= x4; j++)
+						{
+							if (board[i][j] != 2)
+							{
+								board[i][j] += 2;
+							}
+						}
+					}
+				}
+
+				else if (x4 < x3)
+				{
+					x3 += 1;
+
+					x4 += 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = 0; j <= 29; j++)
+						{
+							if (board[i][j] != 2)
+							{
+								board[i][j] += 2;
+							}
+						}
+					}
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x4 + 1; j < x3; j++)
+						{
+							board[i][j] -= 2;
+						}
+					}
+				}
+			}
+
+			else if (order == 'W')
+			{
+				for (int i = y3; i <= y4; i++)
+				{
+					for (int j = x3; j <= x4; j++)
+					{
+						board[i][j] -= 2;
+					}
+				}
+
+				if (x4 >= x3 && x3 > 0)
+				{
+					x3 -= 1;
+
+					x4 -= 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] += 2;
+						}
+					}
+				}
+
+				else if (x4 == 0 && x3 == 0)
+				{
+					x3 = 29;
+
+					x4 = 29;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						board[i][29] += 2;
+
+						board[i][0] -= 2;
+					}
+				}
+
+				else if (x3 == 0 && x4 != 0)
+				{
+					x3 = 29;
+
+					x4 -= 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = 0; j <= 29; j++)
+						{
+							board[i][j] += 2;
+						}
+					}
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x4 + 1; j < x3; j++)
+						{
+							board[i][j] -= 2;
+						}
+					}
+				}
+
+				else if (x4 == 0 && x3 != 0)
+				{
+					x3 -= 1;
+
+					x4 = 29;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						board[i][0] -= 2;
+
+						for (int j = x3; j <= x4; j++)
+						{
+							if (board[i][j] != 2)
+							{
+								board[i][j] += 2;
+							}
+						}
+					}
+				}
+
+				else if (x4 < x3)
+				{
+					x3 -= 1;
+
+					x4 -= 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = 0; j <= 29; j++)
+						{
+							if (board[i][j] != 2)
+							{
+								board[i][j] += 2;
+							}
+						}
+					}
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x4 + 1; j < x3; j++)
+						{
+							board[i][j] -= 2;
+						}
+					}
+				}
+			}
+
+			else if (order == 'a')
+			{
+				for (int i = y3; i <= y4; i++)
+				{
+					for (int j = x3; j <= x4; j++)
+					{
+						board[i][j] -= 2;
+					}
+				}
+
+				if (y4 >= y3 && y4 < 29)
+				{
+					y3 += 1;
+
+					y4 += 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] += 2;
+						}
+					}
+				}
+
+				else if (y4 == 29 && y3 == 29)
+				{
+					y3 = 0;
+
+					y4 = 0;
+
+					for (int i = x3; i <= x4; i++)
+					{
+						board[0][i] += 2;
+
+						board[29][i] -= 2;
+					}
+				}
+
+				else if (y4 == 29 && y3 != 29)
+				{
+					y3 += 1;
+
+					y4 = 0;
+
+					for (int i = 0; i <= 29; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] += 2;
+						}
+					}
+
+					for (int i = y4 + 1; i < y3; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] -= 2;
+						}
+					}
+				}
+
+				else if (y3 == 29 && y4 != 29)
+				{
+					y3 = 0;
+
+					y4 += 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[29][j] -= 2;
+
+							if (board[i][j] != 2)
+							{
+								board[i][j] += 2;
+							}
+						}
+					}
+				}
+
+				else if (y4 < y3)
+				{
+					y3 += 1;
+
+					y4 += 1;
+
+					for (int i = 0; i <= 29; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							if (board[i][j] != 2)
+							{
+								board[i][j] += 2;
+							}
+						}
+					}
+
+					for (int i = y4 + 1; i < y3; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] -= 2;
+						}
+					}
+				}
+			}
+
+			else if (order == 'A')
+			{
+				for (int i = y3; i <= y4; i++)
+				{
+					for (int j = x3; j <= x4; j++)
+					{
+						board[i][j] -= 2;
+					}
+				}
+
+				if (y4 >= y3 && y3 > 0)
+				{
+					y3 -= 1;
+
+					y4 -= 1;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] += 2;
+						}
+					}
+				}
+
+				else if (y4 == 0 && y3 == 0)
+				{
+					y3 = 29;
+
+					y4 = 29;
+
+					for (int i = x3; i <= x4; i++)
+					{
+						board[29][i] += 2;
+
+						board[0][i] -= 2;
+					}
+				}
+
+				else if (y3 == 0 && y4 != 0)
+				{
+					y3 = 29;
+
+					y4 -= 1;
+
+					for (int i = 0; i <= 29; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] += 2;
+						}
+					}
+
+					for (int i = y4 + 1; i < y3; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] -= 2;
+						}
+					}
+				}
+
+				else if (y4 == 0 && y3 != 0)
+				{
+					y3 -= 1;
+
+					y4 = 29;
+
+					for (int i = y3; i <= y4; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[0][j] -= 2;
+
+							if (board[i][j] != 2)
+							{
+								board[i][j] += 2;
+							}
+						}
+					}
+				}
+
+				else if (y4 < y3)
+				{
+					y3 -= 1;
+
+					y4 -= 1;
+
+					for (int i = 0; i <= 29; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							if (board[i][j] != 2)
+							{
+								board[i][j] += 2;
+							}
+						}
+					}
+
+					for (int i = y4 + 1; i < y3; i++)
+					{
+						for (int j = x3; j <= x4; j++)
+						{
+							board[i][j] -= 2;
+						}
+					}
+				}
+			}
+
+			else if (order == 'd')
+			{
+				for (int i = y3; i <= y4; i++)
+				{
+					for (int j = x3; j <= x4; j++)
+					{
+						board[i][j] -= 2;
+					}
+				}
+
+				if (x3 > 0)
+				{
+					x3 -= 1;
+				}
+
+				if (x4 < 29)
+				{
+					x4 += 1;
+				}
+
+				if (y3 > 0)
+				{
+					y3 -= 1;
+				}
+
+				if (y4 < 29)
+				{
+					y4 += 1;
+				}
+
+				for (int i = y3; i <= y4; i++)
+				{
+					for (int j = x3; j <= x4; j++)
+					{
+						board[i][j] += 2;
+					}
+				}
+			}
+
+			else if (order == 'D')
+			{
+				for (int i = y3; i <= y4; i++)
+				{
+					for (int j = x3; j <= x4; j++)
+					{
+						board[i][j] -= 2;
+					}
+				}
+
+				if (x3 + 1 == x4)
+				{
+					x3 += 1;
+				}
+
+				else if (x3 != x4)
+				{
+					x3 += 1;
+
+					x4 -= 1;
+				}
+
+				if (y3 + 1 == y4)
+				{
+					y3 += 1;
+				}
+
+				else if (y3 != y4)
+				{
+					y3 += 1;
+
+					y4 -= 1;
+				}
+
+				for (int i = y3; i <= y4; i++)
+				{
+					for (int j = x3; j <= x4; j++)
+					{
+						board[i][j] += 2;
 					}
 				}
 			}
@@ -534,6 +1176,20 @@ void PrintBoard(int n[30][30])
 			if (board[i][j] == 1)
 			{
 				printf("0 ");
+			}
+
+			else if (board[i][j] == 2)
+			{
+				printf("X ");
+			}
+
+			else if (board[i][j] == 3)
+			{
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+
+				printf("# ");
+
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 			}
 
 			else
