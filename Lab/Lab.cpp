fa408a9637f057e4a02def7ca58ec10f45123b55
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-09-26 13:58:10 +0900

    키보드 입력 추가

--- Lab/Lab.cpp
+++ Lab/Lab.cpp
@@ -4,19 +4,17 @@
 #include <gl/freeglut_ext.h> 
 
 GLvoid drawScene(GLvoid);
-
 GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
 
 int main(int argc, char** argv)
 {
-	//--- 윈도우 생성하기
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(800, 600);
 	glutCreateWindow("Example1");
 
-	//--- GLEW 초기화하기
 	glewExperimental = GL_TRUE;
 
 	if (glewInit() != GLEW_OK)
@@ -28,6 +26,7 @@ int main(int argc, char** argv)
 		std::cout << "GLEW Initialized\n";
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
 	glutMainLoop();
 }
 
@@ -35,12 +34,23 @@ GLvoid drawScene()
 {
 	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	// 그리기 부분 구현: 그리기 관련 부분이 여기에포함된다.
 	glutSwapBuffers();
 }
 
 GLvoid Reshape(int w, int h)
 {
 	glViewport(0, 0, w, h);
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 'c':
+
+	case 'm':
+
+	case 'y':
+
+	}
+	glutPostRedisplay();
 }
