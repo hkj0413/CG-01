d966ee3cfafe602bdd6872bbe071ef04e7eaee64
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-09-27 18:35:40 +0900

    1-2 준비

--- Lab/Lab.cpp
+++ Lab/Lab.cpp
@@ -6,20 +6,17 @@
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
-GLvoid Keyboard(unsigned char key, int x, int y);
-GLvoid TimerFunction(int value);
+GLvoid Mouse(int button, int state, int x, int y);
 
-float R = 1.0, G = 1.0, B = 1.0;
-
-bool repeat = false;
+float R = 0.5, G = 0.5, B = 0.5;
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("Example1");
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("Example");
 
 	glewExperimental = GL_TRUE;
 
@@ -32,7 +29,7 @@ int main(int argc, char** argv)
 		std::cout << "GLEW Initialized\n";
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
-	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(Mouse);
 	glutMainLoop();
 }
 
@@ -40,6 +37,10 @@ GLvoid drawScene()
 {
 	glClearColor(R, G, B, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(0.0, 0.0, 0.0);
+	glRectf(-0.5, -0.5, 0.5, 0.5);
+
 	glutSwapBuffers();
 }
 
@@ -48,53 +49,10 @@ GLvoid Reshape(int w, int h)
 	glViewport(0, 0, w, h);
 }
 
-GLvoid Keyboard(unsigned char key, int x, int y)
-{
-	switch (key) 
-	{
-	case 'c':
-		R = 0.0, G = 1.0, B = 1.0;
-		break;
-	case 'm':
-		R = 1.0, G = 0.0, B = 1.0;
-		break;
-	case 'y':
-		R = 1.0, G = 1.0, B = 0.0;
-		break;
-	case 'w':
-		R = 1.0, G = 1.0, B = 1.0;
-		break;
-	case 'k':
-		R = 0.0, G = 0.0, B = 0.0;
-		break;
-	case 'a':
-		glutTimerFunc(100, TimerFunction, 1);
-		break;
-	case 't':
-		glutTimerFunc(100, TimerFunction, 1);
-		repeat = true;
-		break;
-	case 's':
-		repeat = false;
-		break;
-	case 'q':
-		glutLeaveMainLoop();
-	}
-
-	glutPostRedisplay();
-}
-
-GLvoid TimerFunction(int value)
+GLvoid Mouse(int button, int state, int x, int y)
 {
 	std::random_device rd;
 	std::mt19937 mt(rd());
 	std::normal_distribution<float> dis(0, 1);
 	R = dis(mt), B = dis(mt), G = dis(mt);
-
-	glutPostRedisplay();
-
-	if (repeat)
-	{
-		glutTimerFunc(100, TimerFunction, 1);
-	}
 }
