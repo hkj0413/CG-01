a5f7599890b1bd5b89a745c030f17dd40535ea34
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-09-27 19:32:54 +0900

    사각형 4개 추가

--- Lab/Lab.cpp
+++ Lab/Lab.cpp
@@ -10,12 +10,29 @@ GLvoid Mouse(int button, int state, int x, int y);
 
 float R = 0.5, G = 0.5, B = 0.5;
 
+struct Rect
+{
+	float x1;
+	float y1;
+	float x2;
+	float y2;
+	float R;
+	float G;
+	float B;
+
+}typedef Rect;
+
+Rect rect[4] = { -0.75, 0.75, -0.25, 0.25, 1.0, 0.75, 0.0, 
+				  0.25, 0.25, 0.75, 0.75, 0.25, 1.0, 0.0,
+				 -0.75, -0.75, -0.25, -0.25, 1.0, 0.5, 0.0, 
+				  0.25, -0.25, 0.75, -0.75, 0.5, 0.75, 1.0 };
+
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(800, 800);
+	glutInitWindowSize(1000, 1000);
 	glutCreateWindow("Example");
 
 	glewExperimental = GL_TRUE;
@@ -38,8 +55,11 @@ GLvoid drawScene()
 	glClearColor(R, G, B, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(0.0, 0.0, 0.0);
-	glRectf(-0.5, -0.5, 0.5, 0.5);
+	for (int i = 0; i < 4; i++)
+	{
+		glColor3f(rect[i].R, rect[i].G, rect[i].B);
+		glRectf(rect[i].x1, rect[i].y1, rect[i].x2, rect[i].y2);
+	}
 
 	glutSwapBuffers();
 }
