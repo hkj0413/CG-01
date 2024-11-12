3f6ce6b141b525faddbbec7dce2e5961e30b1538
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-11-12 15:39:41 +0900

    코드 간소화

--- 3-13/3-13.cpp
+++ 3-13/3-13.cpp
@@ -9,25 +9,6 @@
 #include <gl/glm/glm.hpp>
 #include <gl/glm/ext.hpp>
 #include <gl/glm/gtc/matrix_transform.hpp>
-#include <random>
-
-typedef struct
-{
-	float x, y, z;
-} Vertex;
-
-typedef struct
-{
-	unsigned int v1, v2, v3;
-} Face;
-
-typedef struct
-{
-	Vertex* vertices;
-	size_t vertex_count;
-	Face* faces;
-	size_t face_count;
-} Model;
 
 void InitBuffer();
 void make_vertexShaders();
@@ -36,18 +17,11 @@ GLuint make_shaderProgram();
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
-void read_newline(char* str);
-void read_obj_file(const char* filename, Model* model);
 
 GLint width, height;
 GLuint shaderProgramID;
 GLuint vertexShader;
 GLuint fragmentShader;
-
-std::random_device rd;
-std::mt19937 mt(rd());
-std::uniform_int_distribution<int>fea(11, 25);
-std::uniform_int_distribution<int>mut(26, 31);
 
 char* filetobuf(const char* file);
 
@@ -345,65 +319,4 @@ char* filetobuf(const char* file)
 	fclose(fptr);
 	buf[length] = 0;
 	return buf;
-}
-
-void read_newline(char* str)
-{
-	char* pos;
-	if ((pos = strchr(str, '\n')) != NULL)
-		*pos = '\0';
-}
-
-void read_obj_file(const char* filename, Model* model)
-{
-	FILE* file;
-	fopen_s(&file, filename, "r");
-
-	if (!file)
-	{
-		perror("Error opening file");
-		exit(EXIT_FAILURE);
-	}
-
-	char line[600];
-
-	model->vertex_count = 0;
-	model->face_count = 0;
-
-	while (fgets(line, sizeof(line), file))
-	{
-		read_newline(line);
-		if (line[0] == 'v' && line[1] == ' ')
-			model->vertex_count++;
-		else if (line[0] == 'f' && line[1] == ' ')
-			model->face_count++;
-	}
-
-	fseek(file, 0, SEEK_SET);
-	model->vertices = (Vertex*)malloc(model->vertex_count * sizeof(Vertex));
-	model->faces = (Face*)malloc(model->face_count * sizeof(Face));
-	size_t vertex_index = 0;    size_t face_index = 0;
-
-	while (fgets(line, sizeof(line), file))
-	{
-		read_newline(line);
-		if (line[0] == 'v' && line[1] == ' ')
-		{
-			int result = sscanf_s(line + 2, "%f %f %f", &model->vertices[vertex_index].x,
-				&model->vertices[vertex_index].y,
-				&model->vertices[vertex_index].z);
-			vertex_index++;
-		}
-		else if (line[0] == 'f' && line[1] == ' ')
-		{
-			unsigned int v1, v2, v3;
-			int result = sscanf_s(line + 2, "%u %u %u", &v1, &v2, &v3);
-			model->faces[face_index].v1 = v1 - 1;
-			model->faces[face_index].v2 = v2 - 1;
-			model->faces[face_index].v3 = v3 - 1;
-			face_index++;
-		}
-	}
-
-	fclose(file);
 }
